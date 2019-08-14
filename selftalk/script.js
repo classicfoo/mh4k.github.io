3470339fe99e0307ac4c27b89f40427f94277832
michaelhuynh4000 <michaelhuynh4000@gmail.com>
2019-08-14 17:14:04 +1000

    Add files via upload

---
+++ selftalk/script.js
@@ -0,0 +1,37 @@
+﻿console.log("Hello")
+
+var input = document.getElementById("input_box");
+//input.value = "aaa";
+
+
+var counter = 1;
+
+
+function test() {
+
+	var m1 = document.createElement("p");
+	m1.setAttribute("id", "m1");
+
+	var m2 = document.createElement("p");
+	m2.setAttribute("id", "m2");
+
+	// var box = m1;
+	//
+	// if (counter / 2 === 0) {
+	//   box = m1
+	// } else {
+	//   box = m2;
+	// }
+
+	var box = counter % 2 == 0 ? m1 : m2 ;
+	box.innerHTML = input.value
+
+
+	document.getElementById("body").appendChild(box)
+
+
+	counter++;
+
+	input.value = "";
+
+}
